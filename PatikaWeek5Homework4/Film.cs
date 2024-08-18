dfc4d73c69683af038664889282391ad765fa294
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-19 01:11:03 +0300

    Add project files.

---
+++ PatikaWeek5Homework4/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek5Homework4
+{
+    internal class Film
+    {
+        public string Name{ get; set; }
+        public double ImdbRating { get; set; }
+
+        public void FilmInfo()
+        {
+            Console.WriteLine($"Film Adı..:{Name} Imdb Puanı..:{ImdbRating}");
+        }
+    }
+}
---
+++ PatikaWeek5Homework4/Program.cs
@@ -0,0 +1,62 @@
+﻿using PatikaWeek5Homework4;
+//Film tipinde liste tanımlanıyor..
+List<Film> films = new List<Film>();
+
+
+ while (true)
+{
+        Film film = new Film();
+        Console.WriteLine("\nLütfen film adı giriniz..: ");
+        film.Name = Console.ReadLine();
+        Console.WriteLine("Lütfen filmin IMDB puanını giriniz..:");
+ImdbGo: if (Double.TryParse(Console.ReadLine(), out double ImdbRating))
+        {
+            film.ImdbRating = ImdbRating;
+        }
+        else 
+        {
+            Console.WriteLine("Geçersiz giriş.Lütfen IMDB puanını giriniz..: ");
+            goto ImdbGo;
+        }
+        films.Add(film);//listeye yeni film bilgisi ekleniyor.
+
+filmGo: Console.WriteLine("\nYeni bir film eklemek ister misiniz?(e/h)");
+    var continueFilm = Console.ReadLine().ToLower();
+    if (continueFilm == "h")
+    {
+        break;
+    }
+    if (continueFilm != "e")
+    { 
+        Console.WriteLine("Geçersiz giriş.");
+        goto filmGo;
+    }
+}
+
+
+ //Listedeki tüm filimler ekleniyor.
+Console.WriteLine("\nEklediğiniz filmlerin listelesi.....:");
+ foreach(Film film1 in films)
+{
+   film1.FilmInfo();
+
+}
+
+
+
+Console.WriteLine("\n Imdb puanı 4 ile 9 arasında olan filmlerin listelesi..:");
+var imdbFilms=films.Where(f=> f.ImdbRating >= 4 && f.ImdbRating <= 9);// 4 ile 9 puan arası almış filmleri numaralandırıyor
+foreach (Film film1 in imdbFilms)
+{
+    
+        film1.FilmInfo();
+
+}
+
+//
+Console.WriteLine("\n İsmi 'A' ile başlayan filmler ve imdb puanları listelesi.....:");
+var aLetterFilms = films.Where(f => f.Name.StartsWith("A",StringComparison.CurrentCultureIgnoreCase));// A ile başlayan filmleri numaralandırıyor
+foreach (Film film1 in aLetterFilms)
+{        
+        film1.FilmInfo();
+}
